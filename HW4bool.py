0f14f35c3fa8f77129e6694d6b4b8edff59983cf
emadzabad972 <emadzabad972@gmail.com>
2024-09-20 13:12:47 +0300

    Add files via upload

---
+++ HW4bool.py
@@ -0,0 +1,44 @@
+# start
+from operator import truediv
+
+if 4 < 9 : true
+
+if (2*3+4)*(7+7) : true
+
+if 18 + 18 : true
+
+if 10 == 10 : true
+
+if 10 == 10 and 20 == 30 : false
+
+if 10 == 10 or 20 == 30 : true
+
+if 20 == 30 or 10 == 10 : true
+
+if not 3 : false
+
+if 3 : true
+
+if (33>20)and(2<12)and 10 : true
+
+if true and true : true
+
+if true and false : false
+
+if true or false : true
+
+if false or true : true
+
+if (not 10)and(10) : false
+
+if (not 10)and(not 10) : false
+
+if 5 != 5 and 5 == 5 : false
+
+if 2 == 2 or 3 == 3 : true
+
+if 2 == 2 and 3 == 3 : true
+
+if 40 == 30 and 1 >= 4 : false
+
+if 13 >= 3 or 47 >= 5 : true
